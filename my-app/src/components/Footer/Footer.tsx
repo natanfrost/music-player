8d975139c96b63e0544dca9a42979b2f9627dd09
Diamond <nataniel.r.v@gmail.com>
2021-07-08 14:44:03 -0300

    fixed space between icons on footer

--- my-app/src/components/Footer/Footer.tsx
+++ my-app/src/components/Footer/Footer.tsx
@@ -5,18 +5,27 @@ import { FiSearch } from 'react-icons/fi/'
 import { VscLibrary } from 'react-icons/vsc'
 import { FaSpotify } from 'react-icons/fa'
 import { NavLink } from 'react-router-dom';
-import Home from '../Home/Home';
-import Configuration from '../Configuration/Configuration';
+import { IconContext } from 'react-icons/lib';
 
 export default function Footer() {
     return (
         <footer className="Footer">
-            <NavLink to="/">
-                <MdHome className="svg-footer"/>
-            </NavLink>
-            <FiSearch className="svg-footer" />
-            <VscLibrary className="svg-footer" />
-            <FaSpotify className="svg-footer" />
+            <IconContext.Provider value={{ size: '35px', style: { marginTop: '10px' } }}>
+                <div>
+                    <NavLink to="/" >
+                        <MdHome />
+                    </NavLink>
+                </div>
+                <div>
+                    <FiSearch />
+                </div>
+                <div>
+                    <VscLibrary />
+                </div>
+                <div>
+                    <FaSpotify />
+                </div>
+            </IconContext.Provider>
         </footer>
     )
 }
