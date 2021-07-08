7f251f4e82ed673ffa9da68164c39df69160c6f9
Diamond <nataniel.r.v@gmail.com>
2021-07-08 14:47:31 -0300

    fixed header icons

--- my-app/src/components/Head/Head.tsx
+++ my-app/src/components/Head/Head.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './style.css';
 import { BsGear } from "react-icons/bs";
 import { MdReplay } from "react-icons/md";
+import { IconContext } from 'react-icons/lib';
 
 export default function Head() {
     return(
         <div className='container'>
             <h2>Good morning!</h2>
-            <MdReplay />
-            <BsGear />
+            <IconContext.Provider value={{ size: '25px', style: { marginRight: '10px' } }}>
+                <MdReplay />
+                <BsGear />
+            </IconContext.Provider>
         </div>
     )
 }
