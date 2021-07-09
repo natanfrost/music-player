cd879084ac4b2684bc31c89b178da67bd3df0424
Diamond <nataniel.r.v@gmail.com>
2021-07-09 10:59:39 -0300

    adjusted items aligment in profile details

--- my-app/src/components/Configuration/Configuration.tsx
+++ my-app/src/components/Configuration/Configuration.tsx
@@ -17,11 +17,14 @@ export default function Configuration() {
             </div>
 
             <div className="container">
-            
+
                 <div className="profile">
-                    <img src={profile} alt="" />
+                    <div className="profile-img">
+                        <img src={profile} alt="" />
+                    </div>
+
                     <div className="profile-detail">
-                        <h2>Your Name</h2>
+                        <h3>Your Name</h3>
                         <span>See profile</span>
                     </div>
                     <div className="profile-icon">
@@ -34,16 +37,16 @@ export default function Configuration() {
                 <h5>E-mail</h5>
                 <span>email@email.com</span>
                 <h5>Data economy</h5>
-                <h5>Audio Quality</h5>                
+                <h5>Audio Quality</h5>
                 <div className="toggle">
                     <div>
                         <span>Sets your audio quality to low</span>
                     </div>
                     <div>
-                    <label className="switch">
-                        <input type="checkbox"/>
-                        <span className="slider round"></span>
-                    </label></div>
+                        <label className="switch">
+                            <input type="checkbox" />
+                            <span className="slider round"></span>
+                        </label></div>
                 </div>
             </div>
         </div>
