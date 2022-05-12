0b0526385d72695f327d604202b2e7805427d115
Diamond <nataniel.r.v@gmail.com>
2022-05-12 10:35:53 -0300

    demo link and mock db added

---
+++ my-app/src/db/mock.js
@@ -0,0 +1,547 @@
+const  db = {
+    'playlists': [
+        {
+            'name': 'Musics',
+            'cover': 'rhcp.jpg',
+            'artists': [
+                {
+                    'name': 'Red Hot Chili Peppers',
+                    'albums': [
+                        {
+                            'name': 'Californication',
+                            'songs': [
+                                {
+                                    'name': 'Californication',
+                                    'length': 436
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'System of a down',
+                    'albums': [
+                        {
+                            'name': 'Toxicity',
+                            'songs': [
+                                {
+                                    'name': 'Toxicity',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Avenged Sevenfold',
+                    'albums': [
+                        {
+                            'name': 'Nightmare',
+                            'songs': [
+                                {
+                                    'name': 'Nightmare',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Howard Shore',
+                    'albums': [
+                        {
+                            'name': 'LOTR OST',
+                            'songs': [
+                                {
+                                    'name': 'Concerning Hobbits',
+                                    'length': 465
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Leonard Cohen',
+                    'albums': [
+                        {
+                            'name': 'Various Positions',
+                            'songs': [
+                                {
+                                    'name': 'Hallelujah',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Lindsey Stirling',
+                    'albums': [
+                        {
+                            'name': 'I am Lindsey',
+                            'songs': [
+                                {
+                                    'name': 'Roundtable Rival',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'name': 'Rock Classics',
+            'cover': 'a7x.jpg',
+            'artists': [
+                {
+                    'name': 'Red Hot Chili Peppers',
+                    'albums': [
+                        {
+                            'name': 'Californication',
+                            'songs': [
+                                {
+                                    'name': 'Californication',
+                                    'length': 436
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'System of a down',
+                    'albums': [
+                        {
+                            'name': 'Toxicity',
+                            'songs': [
+                                {
+                                    'name': 'Toxicity',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Avenged Sevenfold',
+                    'albums': [
+                        {
+                            'name': 'Nightmare',
+                            'songs': [
+                                {
+                                    'name': 'Nightmare',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Howard Shore',
+                    'albums': [
+                        {
+                            'name': 'LOTR OST',
+                            'songs': [
+                                {
+                                    'name': 'Concerning Hobbits',
+                                    'length': 465
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Leonard Cohen',
+                    'albums': [
+                        {
+                            'name': 'Various Positions',
+                            'songs': [
+                                {
+                                    'name': 'Hallelujah',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Lindsey Stirling',
+                    'albums': [
+                        {
+                            'name': 'I am Lindsey',
+                            'songs': [
+                                {
+                                    'name': 'Roundtable Rival',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'name': 'Daily Mix',
+            'cover': 'soundgarden.jpg',
+            'artists': [
+                {
+                    'name': 'Red Hot Chili Peppers',
+                    'albums': [
+                        {
+                            'name': 'Californication',
+                            'songs': [
+                                {
+                                    'name': 'Californication',
+                                    'length': 436
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'System of a down',
+                    'albums': [
+                        {
+                            'name': 'Toxicity',
+                            'songs': [
+                                {
+                                    'name': 'Toxicity',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Avenged Sevenfold',
+                    'albums': [
+                        {
+                            'name': 'Nightmare',
+                            'songs': [
+                                {
+                                    'name': 'Nightmare',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Howard Shore',
+                    'albums': [
+                        {
+                            'name': 'LOTR OST',
+                            'songs': [
+                                {
+                                    'name': 'Concerning Hobbits',
+                                    'length': 465
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Leonard Cohen',
+                    'albums': [
+                        {
+                            'name': 'Various Positions',
+                            'songs': [
+                                {
+                                    'name': 'Hallelujah',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Lindsey Stirling',
+                    'albums': [
+                        {
+                            'name': 'I am Lindsey',
+                            'songs': [
+                                {
+                                    'name': 'Roundtable Rival',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'name': '60s Rcok',
+            'cover': 'sp.png',
+            'artists': [
+                {
+                    'name': 'Red Hot Chili Peppers',
+                    'albums': [
+                        {
+                            'name': 'Californication',
+                            'songs': [
+                                {
+                                    'name': 'Californication',
+                                    'length': 436
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'System of a down',
+                    'albums': [
+                        {
+                            'name': 'Toxicity',
+                            'songs': [
+                                {
+                                    'name': 'Toxicity',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Avenged Sevenfold',
+                    'albums': [
+                        {
+                            'name': 'Nightmare',
+                            'songs': [
+                                {
+                                    'name': 'Nightmare',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Howard Shore',
+                    'albums': [
+                        {
+                            'name': 'LOTR OST',
+                            'songs': [
+                                {
+                                    'name': 'Concerning Hobbits',
+                                    'length': 465
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Leonard Cohen',
+                    'albums': [
+                        {
+                            'name': 'Various Positions',
+                            'songs': [
+                                {
+                                    'name': 'Hallelujah',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Lindsey Stirling',
+                    'albums': [
+                        {
+                            'name': 'I am Lindsey',
+                            'songs': [
+                                {
+                                    'name': 'Roundtable Rival',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'name': 'Discoveries',
+            'cover': 'a7x.jpg',
+            'artists': [
+                {
+                    'name': 'Red Hot Chili Peppers',
+                    'albums': [
+                        {
+                            'name': 'Californication',
+                            'songs': [
+                                {
+                                    'name': 'Californication',
+                                    'length': 436
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'System of a down',
+                    'albums': [
+                        {
+                            'name': 'Toxicity',
+                            'songs': [
+                                {
+                                    'name': 'Toxicity',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Avenged Sevenfold',
+                    'albums': [
+                        {
+                            'name': 'Nightmare',
+                            'songs': [
+                                {
+                                    'name': 'Nightmare',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Howard Shore',
+                    'albums': [
+                        {
+                            'name': 'LOTR OST',
+                            'songs': [
+                                {
+                                    'name': 'Concerning Hobbits',
+                                    'length': 465
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Leonard Cohen',
+                    'albums': [
+                        {
+                            'name': 'Various Positions',
+                            'songs': [
+                                {
+                                    'name': 'Hallelujah',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Lindsey Stirling',
+                    'albums': [
+                        {
+                            'name': 'I am Lindsey',
+                            'songs': [
+                                {
+                                    'name': 'Roundtable Rival',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'name': 'Explore',
+            'cover': 'blink.jpg',
+            'artists': [
+                {
+                    'name': 'Red Hot Chili Peppers',
+                    'albums': [
+                        {
+                            'name': 'Californication',
+                            'songs': [
+                                {
+                                    'name': 'Californication',
+                                    'length': 436
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'System of a down',
+                    'albums': [
+                        {
+                            'name': 'Toxicity',
+                            'songs': [
+                                {
+                                    'name': 'Toxicity',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Avenged Sevenfold',
+                    'albums': [
+                        {
+                            'name': 'Nightmare',
+                            'songs': [
+                                {
+                                    'name': 'Nightmare',
+                                    'length': 541
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Howard Shore',
+                    'albums': [
+                        {
+                            'name': 'LOTR OST',
+                            'songs': [
+                                {
+                                    'name': 'Concerning Hobbits',
+                                    'length': 465
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Leonard Cohen',
+                    'albums': [
+                        {
+                            'name': 'Various Positions',
+                            'songs': [
+                                {
+                                    'name': 'Hallelujah',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Lindsey Stirling',
+                    'albums': [
+                        {
+                            'name': 'I am Lindsey',
+                            'songs': [
+                                {
+                                    'name': 'Roundtable Rival',
+                                    'length': 413
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+    
+}
+
+export default db;
