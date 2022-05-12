68a5fa9636f3e2fea6897291eb0d483fd58cb4f3
Diamond <nataniel.r.v@gmail.com>
2022-05-12 09:59:06 -0300

    created function to personalized welcome message

--- my-app/src/components/Head/Head.tsx
+++ my-app/src/components/Head/Head.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import './style.css';
 import { BsGear } from "react-icons/bs";
 import { MdReplay } from "react-icons/md";
 import { IconContext } from 'react-icons/lib';
 import { NavLink } from 'react-router-dom';
 import { useHistory } from 'react-router';
+import { useEffect, useState } from 'react';
 
 export default function Head() {
+    const [welcomeMessage, setWelcomeMessage] = useState('');
 
     const history = useHistory();
 
@@ -14,9 +15,25 @@ export default function Head() {
         history.go(0);
     }
 
+    const getWelcomeMessage = () => {
+        const now = new Date().getHours();
+
+        if (now > 4 && now <= 12) {
+            setWelcomeMessage('Good morning!')
+        } else if (now > 12 && now <= 18) {
+            setWelcomeMessage('Good afternoom!')
+        } else {
+            setWelcomeMessage('Good night!')
+        }
+    }
+
+    useEffect(() => {
+        getWelcomeMessage();
+    })
+
     return(
         <div className='container'>
-            <h2>Good morning!</h2>
+            <h2>{welcomeMessage}</h2>
             <IconContext.Provider value={{ size: '25px', style: { marginRight: '10px' } }}>
                 <MdReplay onClick={() => handleReload()} cursor={'pointer'}/>
                 <NavLink to='/configuration'>
