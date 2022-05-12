9340b972c10a6008bf50b6616c391893f49887a3
Diamond <nataniel.r.v@gmail.com>
2022-05-12 09:44:23 -0300

    set event to reload page in reload icon and changed cursor to pointer

--- my-app/src/components/Head/Head.tsx
+++ my-app/src/components/Head/Head.tsx
@@ -4,13 +4,21 @@ import { BsGear } from "react-icons/bs";
 import { MdReplay } from "react-icons/md";
 import { IconContext } from 'react-icons/lib';
 import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
 export default function Head() {
+
+    const history = useHistory();
+
+    const handleReload = () => {
+        history.go(0);
+    }
+
     return(
         <div className='container'>
             <h2>Good morning!</h2>
             <IconContext.Provider value={{ size: '25px', style: { marginRight: '10px' } }}>
-                <MdReplay />
+                <MdReplay onClick={() => handleReload()} cursor={'pointer'}/>
                 <NavLink to='/configuration'>
                     <BsGear />
                 </NavLink>
